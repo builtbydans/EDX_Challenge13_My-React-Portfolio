25b6a393458519a2107982b700c0ba7235aa6513
Danish Shafi <danish.shafi@live.co.uk>
2023-03-07 17:45:56 +0000

    Components create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 
 function App() {
   return (
     <div className="App">
       <h1>My React Portfolio</h1>
+      <Header />
+      <Home />
+      <Contact />
+      <ProjectsPage />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <div>
+      <h1>This is the Header component</h1>
+    </div>
+  )
+}
+
+export default Header
